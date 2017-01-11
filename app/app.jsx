70ec80c336ffd3829ad3c4a5813840d90147bbbc
Jasonjys <jasonjys0511@gmail.com>
2017-01-11 00:12:38 -0500

    added nav bar and router setup(still prefer Ember at this moment)

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Example = require('Example');
 
 ReactDOM.render(
-  <h1>Boilerpate app!</h1>,
+  <Router history={hashHistory}>
+  	<Route path="/" component={Main}>
+  		<IndexRoute component={Weather}/>
+  		<Route path="about" component={About}/>
+  		<Route path="example" component={Example}/>
+  	</Route>
+  </Router>,
   document.getElementById('app')
 );
