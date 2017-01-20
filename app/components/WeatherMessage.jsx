82e60e94d5cb2a28a2427913acdfd2678ff19deb
Jasonjys <jasonjys0511@gmail.com>
2017-01-20 00:50:46 -0500

    changed syntax

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,16 +1,11 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-	render: function () {
-		// get location and temp from the parent component Weather
-		// Note: pull values from this.props not this.states
-		var { location, temp } = this.props;
-
-
-		return (
-			<h3>The temperature in {location} is {temp}°C</h3>
-		);
-	}
-});
+// get location and temp from the parent component Weather
+// Note: pull values from this.props not this.states
+var WeatherMessage = ({ location, temp }) => {
+	return (
+		<h3>The temperature in {location} is {temp}°C</h3>
+	);
+};
 
 module.exports = WeatherMessage;
