cfaf431a7cc59ce168a28b7674d66af316202b59
Jasonjys <jasonjys0511@gmail.com>
2017-01-20 16:42:20 -0500

    added styles for pages

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+const { Link } = require('react-router');
 
 var Example = (props) => {
-	return (
-		<h3>Example Component</h3>
-	);
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Shanghai'>Shanghai, China</Link>
+        </li>
+        <li>
+          <Link to='/?location=Ottawa'>Ottawa, Canada</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 module.exports = Example;
